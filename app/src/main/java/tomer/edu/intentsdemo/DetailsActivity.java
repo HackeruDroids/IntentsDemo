646cb4cc70ac0698b15a27fe231d27cf2321c3b0
hackerudroids <tomerjava@gmail.com>
2017-04-24 21:31:03 +0300

    intents

--- app/src/main/java/tomer/edu/intentsdemo/DetailsActivity.java
+++ app/src/main/java/tomer/edu/intentsdemo/DetailsActivity.java
@@ -1,8 +1,11 @@
 package tomer.edu.intentsdemo;
 
 import android.content.Intent;
+import android.os.Build;
+import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -31,5 +34,37 @@ public class DetailsActivity extends AppCompatActivity implements TimePicker.OnT
     public void onTimeChanged(TimePicker timePicker, int hour, int minutes) {
         //
 
+    }
+
+    //<uses-permission ...
+    // <uses-permission android:name="com.android.alarm.permission.SET_ALARM" />
+
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_SKIP_UI , true)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+
+
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
+
+    public void setAlarm(View view) {
+        int hour = 0;
+        int minutes = 0;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            hour = picker.getHour();
+            minutes = picker.getMinute();
+        }else{
+            hour = picker.getCurrentHour();
+            minutes = picker.getCurrentHour();
+        }
+
+
+
+        createAlarm("Yoga Class", hour, minutes);
     }
 }
